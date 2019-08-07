6c6dd1afce49e2668827c204990ccda80f2be3de
Milton Fernado Molloja <milton.molloja@gmail.com>
2019-08-07 16:00:09 -0300

    Init

---
+++ VidlyByYo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyByYo.Startup))]
+namespace VidlyByYo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
