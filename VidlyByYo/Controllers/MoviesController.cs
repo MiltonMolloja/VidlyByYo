8987cdce662974f9fb19dcb3a2621f0d694ea330
Milton Fernado Molloja <milton.molloja@gmail.com>
2019-08-07 18:04:31 -0300

    Avance

---
+++ VidlyByYo/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyByYo.Models;
+
+namespace VidlyByYo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Id = 1,  Name = "Shrek!!" };
+
+            return View(movie);
+        }
+    }
+}
