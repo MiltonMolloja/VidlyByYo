00406ea8d15ae6e24061c1373d58b643e138218e
Milton Fernado Molloja <milton.molloja@gmail.com>
2019-08-09 12:06:25 -0300

    Base Datos Finis

--- VidlyByYo/Controllers/MoviesController.cs
+++ VidlyByYo/Controllers/MoviesController.cs
@@ -4,29 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VidlyByYo.Models;
+using System.Data.Entity;
 
 
 namespace VidlyByYo.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
 
         public ViewResult Index() {
-            var movie = GetMovies();
+            var movie = _context.Movies.Include(c => c.Genre).ToList();
             return View(movie);
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie {Id = 1 , Name = "Shrek"},
-                new Movie {Id = 2 , Name = "Wall-e"},
-                new Movie {Id = 3 , Name = "Irom-Man"},
-                new Movie {Id = 4 , Name = "Hulk"}
-            };
-        }        
-        
+        public ActionResult Details(int id)
+        {            
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+
 
     }
 }
